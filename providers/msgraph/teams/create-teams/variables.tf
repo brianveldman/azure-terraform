f1ac9ad49a7384a5e5d0907a6cbea1dd1d5fd536
Brian Veldman <147204186+brianveldman@users.noreply.github.com>
2025-08-16 12:38:34 +0200

    Create variables.tf

---
+++ providers/msgraph/teams/create-teams/variables.tf
@@ -0,0 +1,14 @@
+variable "teams_displayName" {
+  type        = string
+  description = "Team DisplayName"
+}
+
+variable "teams_description" {
+  type        = string
+  description = "Team created via Terraform"
+}
+
+variable "teams_owner_id" {
+  type        = string
+  description = "Team Owner ID"
+}
