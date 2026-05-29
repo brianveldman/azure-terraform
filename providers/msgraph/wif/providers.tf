ef8abac390456a4a00b8ab4ef8626a36b0ff00d2
Brian Veldman <147204186+brianveldman@users.noreply.github.com>
2026-05-29 07:49:37 +0200

    Add Terraform provider configuration for Azure DevOps and MSGraph

---
+++ providers/msgraph/wif/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "1.10.0"
+    }
+    msgraph = {
+      source = "microsoft/msgraph"
+    }
+  }
+}
